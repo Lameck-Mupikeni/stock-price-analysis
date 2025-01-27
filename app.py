8f1eb1d9da3b10c65e711dbb481de746226f46e6
Lameck-Mupikeni <lameck.mupikeni@gmail.com>
2025-01-27 22:40:01 +0200

    Setting Up Flask

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+from models.prediction_model import fetch_and_predict
+
+app = Flask(__name__)
+
+@app.route('/api/stock', methods=['GET'])
+def get_stock_data():
+    symbol = request.args.get('symbol', 'AAPL')
+    result = fetch_and_predict(symbol)
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
