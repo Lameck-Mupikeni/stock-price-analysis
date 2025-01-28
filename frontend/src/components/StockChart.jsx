a25546463863107ff383cd06dfbfe0a477c5a9e0
FarieB <faraibande@gmail.com>
2025-01-28 21:40:28 +0200

    development

---
+++ frontend/src/components/StockChart.jsx
@@ -0,0 +1,28 @@
+mport React from 'react';
+import { Line } from 'react-chartjs-2';
+
+const StockChart = ({ data, predictions }) => {
+	  const chartData = {
+		      labels: data.dates,
+		      datasets: [
+			            {
+					            label: 'Historical Prices',
+					            data: data.prices,
+					            borderColor: 'blue',
+					            fill: false,
+					          },
+			            {
+					            label: 'Predicted Prices',
+					            data: predictions,
+					            borderColor: 'red',
+					            borderDash: [5, 5],
+					            fill: false,
+					          },
+			          ],
+		    };
+
+	  return <Line data={chartData} />;
+};
+
+export default StockChart;
+
