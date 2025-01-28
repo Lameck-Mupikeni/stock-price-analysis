46a43f1fada966e6de2b084c2e16c6fba08affb5
FarieB <133366471+FarieB@users.noreply.github.com>
2025-01-29 00:44:38 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS  # Import CORS
 from models.prediction_model import fetch_and_predict
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/api/stock', methods=['GET'])
 def get_stock_data():
-    symbol = request.args.get('symbol', 'AAPL')  # Default to 'AAPL' if no symbol provided
+    symbol = request.args.get('symbol', 'AAPL')
     result = fetch_and_predict(symbol)
     return jsonify(result)
 
