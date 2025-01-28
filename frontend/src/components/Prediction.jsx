be347a0fd4fc234db0234d09ec56e766b7667e86
FarieB <faraibande@gmail.com>
2025-01-29 00:06:51 +0200

    update

---
+++ frontend/src/components/Prediction.jsx
@@ -0,0 +1,17 @@
+mport React from 'react';
+
+const Prediction = ({ predictedData }) => {
+	  return (
+		      <div>
+		        <h2>Stock Price Prediction</h2>
+		        <ul>
+		          {predictedData.map((prediction, index) => (
+				            <li key={index}>Predicted Price: {prediction.price}</li>
+				          ))}
+		        </ul>
+		      </div>
+		    );
+};
+
+export default Prediction;
+
